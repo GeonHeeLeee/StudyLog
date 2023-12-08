87cdb770761fd1e4395c672d5f6931dc72fe6e3c
JongMany <blackberry1114@naver.com>
2023-12-08 10:38:32 +0900

    feat: 스터디로그에 시간 보여주는 기능 추가

--- client/src/pages/Profile/StudyLog/StudyLog.tsx
+++ client/src/pages/Profile/StudyLog/StudyLog.tsx
@@ -12,6 +12,7 @@ const today = new Date();
 type StudyLog = {
   date: string;
   count: number;
+  studyTime: string;
 };
 
 export default function StudyLog(props: { timers: Timers[] }) {
@@ -19,6 +20,8 @@ export default function StudyLog(props: { timers: Timers[] }) {
 
   // Tooltip 띄우기 위해 존재하지 않는 날짜 데이터 생성
   const completeStudyLogs = generateYearData(new Date(), timers);
+
+  console.log(timers);
 
   return (
     <div className={styles['studylog-container']}>
@@ -36,7 +39,8 @@ export default function StudyLog(props: { timers: Timers[] }) {
           tooltipDataAttrs={(value: StudyLog) => {
             return {
               'data-tooltip-id': 'heatmap-tooltip',
-              'data-tooltip-content': `${value.date}`,
+              'data-tooltip-content': `${value.date}
+              공부시간: ${value.studyTime}`,
             };
           }}
         />
@@ -60,11 +64,30 @@ const generateYearData = (baseDate: Date, logs: Timers[]) => {
     const dateString = date.toISOString().split('T')[0]; // 날짜를 'yyyy-mm-dd' 형식의 문자열로 변환
 
     const log = logs.find((log) => log.date === dateString);
+    console.log(log);
 
     data.push({
       date: dateString,
       count: log ? log.state : 0,
+      studyTime: log ? convertStudyTime(log?.studyTime) : '0초',
     });
   }
   return data;
+};
+
+const convertStudyTime = (seconds: number | undefined) => {
+  if (!seconds) return '0초';
+  if (seconds < 60) {
+    return `${seconds}초`;
+  } else if (seconds < 3600) {
+    const minutes = parseInt(`${seconds / 60}`);
+    const second = seconds % 60;
+
+    return `${minutes}분 ${second}초`;
+  }
+  const hours = parseInt(`${seconds / (60 * 60)}`);
+  const temp = seconds % (60 * 60);
+  const minutes = parseInt(`${temp / 60}`);
+  const second = temp % 60;
+  return `${hours}시간 ${minutes}분 ${second}초`;
 };

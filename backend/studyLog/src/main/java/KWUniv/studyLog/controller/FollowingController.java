af40dc382dcd42c0fa08e40a6450ef2abf47573a
GeonHeeLeee <ghlee00125@naver.com>
2023-12-08 13:55:38 +0900

    특정 회원 following List 로직 구현

--- backend/studyLog/src/main/java/KWUniv/studyLog/controller/FollowingController.java
+++ backend/studyLog/src/main/java/KWUniv/studyLog/controller/FollowingController.java
@@ -1,7 +1,9 @@
 package KWUniv.studyLog.controller;
 
 import KWUniv.studyLog.DTO.FollowingDTO;
+import KWUniv.studyLog.entity.Following;
 import KWUniv.studyLog.exception.UserNotFoundException;
+import KWUniv.studyLog.repository.FollowingRepository;
 import KWUniv.studyLog.service.FollowingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +11,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,7 +27,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class FollowingController {
 
     private final FollowingService followingService;
-
+    private final FollowingRepository followingRepository;
 
     /*
     해당 유저 팔로우
@@ -45,5 +54,20 @@ public class FollowingController {
         } catch (UserNotFoundException e) {
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
+    }
+
+    /*
+    특정 회원의 팔로잉 리스트
+     */
+    @GetMapping("/following")
+    public ResponseEntity getFollowingList(@RequestParam String selfId) {
+        List<Following> followingList = followingRepository.findBySelfUser_UserId(selfId);
+        Map<String, Object> response = new HashMap<>();
+        List<String> followingIds = followingList.stream()
+                .map(following -> following.getFollowingUser().getUserId())
+                .collect(Collectors.toList());
+        response.put("selfId", selfId);
+        response.put("followingIds",followingIds);
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }
